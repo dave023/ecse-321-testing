1e2242b005b8b8aa06833b16125260bda9e887f0
dave023 <dave023@msn.com>
2011-03-14 15:27:20 -0700

    Edited src/HelloWorld.java via GitHub

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,5 @@
-
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!!!"); // Display the string.
+        System.out.println("Hello World Hello!!!!!"); // Display the string.
     }
 }
