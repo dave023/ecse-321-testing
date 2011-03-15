04341855df48eb80d7022d3f78183fe827889edd
asdfasdfasdf <Dave@Dave-PC>
2011-03-15 13:24:17 -0400

    asdf

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World Hello!!!!!"); // Display the string.
+        System.out.println("Hello World asdfasdfsdfasdfHello!!!!!"); // Display the string.
     }
 }
